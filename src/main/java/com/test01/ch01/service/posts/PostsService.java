a25bb217c3d549a5d2313cb2a1e47e55d3881ee6
cyeongy <l3714520>
2021-05-22 16:36:04 +0900

    ADD API, DTO, Service file
    make Web, Service, Repository Layer
    Web + Service = DTO
    Service + Repository = Domain Model

---
+++ src/main/java/com/test01/ch01/service/posts/PostsService.java
@@ -0,0 +1,18 @@
+package com.test01.ch01.service.posts;
+
+import com.test01.ch01.domain.posts.PostsRepository;
+import com.test01.ch01.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
---
+++ src/main/java/com/test01/ch01/web/PostsApiController.java
@@ -0,0 +1,19 @@
+package com.test01.ch01.web;
+
+import com.test01.ch01.service.posts.PostsService;
+import com.test01.ch01.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+}

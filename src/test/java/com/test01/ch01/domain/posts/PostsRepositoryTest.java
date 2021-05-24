0926dd0dc21e2d35382ec4256f51007a215ff40c
cyeongy <l3714520>
2021-05-24 19:02:54 +0900

    ADD BaseTimeEntity test code
    Entity의 createdDate, modifiedDate 자동관리 클래스 test

--- src/test/java/com/test01/ch01/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/test01/ch01/domain/posts/PostsRepositoryTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -38,5 +39,24 @@ public class PostsRepositoryTest {
         Posts posts = postsList.get(0);
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void BaseTimeEntity_test() {
+        LocalDateTime now = LocalDateTime.of(1996, 6, 12, 0, 0, 0);
+        postsRepository.save(Posts.builder()
+                .title("ju")
+                .content("yyeon")
+                .author("2")
+                .build());
+
+        List<Posts> postsList = postsRepository.findAll();
+
+        Posts posts = postsList.get(0);
+
+        System.out.println(">>>>> createDate=" + posts.getCreatedDate() + " modifiedDate=" + posts.getModifiedDate());
+
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
     }
 }

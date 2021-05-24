9014f4b833ca24b3dda098af6be0760e3d0b2de5
cyeongy <l3714520>
2021-05-24 19:12:23 +0900

    Updat Application.java to use JPAAuditing
    @EnableJPAAuditing 추가

--- src/main/java/com/test01/ch01/Application.java
+++ src/main/java/com/test01/ch01/Application.java
@@ -2,8 +2,9 @@ package com.test01.ch01;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-
+@EnableJpaAuditing
 @SpringBootApplication
 public class Application {
     public static void main(String[] args){

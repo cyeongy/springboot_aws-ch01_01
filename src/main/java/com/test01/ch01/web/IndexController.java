e2be6dc63fa82e5cc92a3c4ebf871666c8921714
cyeongy <cyeongy@gmail.com>
2021-05-31 15:59:37 +0900

    Fix IndexController, posts-update.mustache semantics
    Fix semantics, switch label text and textarea

--- src/main/java/com/test01/ch01/web/IndexController.java
+++ src/main/java/com/test01/ch01/web/IndexController.java
@@ -1,10 +1,12 @@
 package com.test01.ch01.web;
 
 import com.test01.ch01.service.posts.PostsService;
+import com.test01.ch01.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -16,6 +18,14 @@ public class IndexController {
     public String index (Model model){
         model.addAttribute("posts", postsService.findAllDesc());
         return "index";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
     }
 
 
